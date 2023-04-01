f11c0f339aefcd0bb602674e723e6fb5c395fcb9
West <weihouyao@huawei.com>
2023-04-01 15:28:12 +0800

    【修改说明】添加avcodec北向接口错误码
    Signed-off-by: West <weihouyao@huawei.com>

--- interfaces/inner_api/native/avcodec_errors.h
+++ interfaces/inner_api/native/avcodec_errors.h
@@ -28,69 +28,83 @@ using AVCSErrCode = ErrCode;
 // TODO: confirm module offset
 constexpr AVCSErrCode AVCS_MODULE = 0X01000;
 constexpr AVCSErrCode AVCS_ERR_OFFSET = ErrCodeOffset(SUBSYS_MULTIMEDIA, AVCS_MODULE);
-enum AVCodecServiceErrCode : ErrCode {
-    AVCSERR_OK                = ERR_OK,
-    AVCSERR_NO_MEMORY         = AVCS_ERR_OFFSET + ENOMEM, // no memory
-    AVCSERR_INVALID_OPERATION = AVCS_ERR_OFFSET + ENOSYS, // opertation not be permitted
-    AVCSERR_INVALID_VAL       = AVCS_ERR_OFFSET + EINVAL, // invalid argument
-    AVCSERR_UNKNOWN           = AVCS_ERR_OFFSET + 0x200,  // unkown error.
-    AVCSERR_SERVICE_DIED,                                 // avcodec service died
-    AVCSERR_CREATE_REC_ENGINE_FAILED,                     // create recorder engine failed.
-    AVCSERR_CREATE_PLAYER_ENGINE_FAILED,                  // create player engine failed.
-    AVCSERR_CREATE_AVMETADATAHELPER_ENGINE_FAILED,        // create avmetadatahelper engine failed.
-    AVCSERR_CREATE_AVCODEC_ENGINE_FAILED,                 // create avcodec engine failed.
-    AVCSERR_INVALID_STATE,                                // the state is not support this operation.
-    AVCSERR_UNSUPPORT,                                    // unsupport interface.
-    AVCSERR_UNSUPPORT_AUD_SRC_TYPE,                       // unsupport audio source type.
-    AVCSERR_UNSUPPORT_AUD_SAMPLE_RATE,                    // unsupport audio sample rate.
-    AVCSERR_UNSUPPORT_AUD_CHANNEL_NUM,                    // unsupport audio channel.
-    AVCSERR_UNSUPPORT_AUD_ENC_TYPE,                       // unsupport audio encoder type.
-    AVCSERR_UNSUPPORT_AUD_PARAMS,                         // unsupport audio params(other params).
-    AVCSERR_UNSUPPORT_VID_SRC_TYPE,                       // unsupport video source type.
-    AVCSERR_UNSUPPORT_VID_ENC_TYPE,                       // unsupport video encoder type.
-    AVCSERR_UNSUPPORT_VID_PARAMS,                         // unsupport video params(other params).
-    AVCSERR_UNSUPPORT_CONTAINER_TYPE,                     // unsupport container format type.
-    AVCSERR_UNSUPPORT_PROTOCOL_TYPE,                      // unsupport protocol type.
-    AVCSERR_UNSUPPORT_VID_DEC_TYPE,                       // unsupport video decoder type.
-    AVCSERR_UNSUPPORT_AUD_DEC_TYPE,                       // unsupport audio decoder type.
-    AVCSERR_UNSUPPORT_STREAM,                             // internal data stream error.
-    AVCSERR_UNSUPPORT_FILE,                               // this appears to be a text file.
-    AVCSERR_UNSUPPORT_SOURCE,                             // unsupport source type.
-    AVCSERR_AUD_RENDER_FAILED,                            // audio render failed.
-    AVCSERR_AUD_ENC_FAILED,                               // audio encode failed.
-    AVCSERR_VID_ENC_FAILED,                               // video encode failed.
-    AVCSERR_AUD_DEC_FAILED,                               // audio decode failed.
-    AVCSERR_VID_DEC_FAILED,                               // video decode failed.
-    AVCSERR_MUXER_FAILED,                                 // stream avmuxer failed.
-    AVCSERR_DEMUXER_FAILED,                               // stream demuxer or parser failed.
-    AVCSERR_OPEN_FILE_FAILED,                             // open file failed.
-    AVCSERR_FILE_ACCESS_FAILED,                           // read or write file failed.
-    AVCSERR_START_FAILED,                                 // audio/video start failed.
-    AVCSERR_PAUSE_FAILED,                                 // audio/video pause failed.
-    AVCSERR_STOP_FAILED,                                  // audio/video stop failed.
-    AVCSERR_SEEK_FAILED,                                  // audio/video seek failed.
-    AVCSERR_NETWORK_TIMEOUT,                              // network timeout.
-    AVCSERR_NOT_FIND_CONTAINER,                           // not find a demuxer.
-    AVCSERR_DATA_SOURCE_IO_ERROR,                         // avcodec data source IO failed.
-    AVCSERR_DATA_SOURCE_OBTAIN_MEM_ERROR,                 // avcodec data source get mem failed.
-    AVCSERR_DATA_SOURCE_ERROR_UNKNOWN,                    // avcodec data source error unknow.
-    AVCSERR_EXTEND_START      = AVCS_ERR_OFFSET + 0xF000, // extend err start.
-};
+typedef enum AVCodecServiceErrCode : ErrCode {
+    AVCS_ERR_OK                = ERR_OK,
+    AVCS_ERR_NO_MEMORY         = AVCS_ERR_OFFSET + ENOMEM, // no memory
+    AVCS_ERR_INVALID_OPERATION = AVCS_ERR_OFFSET + ENOSYS, // opertation not be permitted
+    AVCS_ERR_INVALID_VAL       = AVCS_ERR_OFFSET + EINVAL, // invalid argument
+    AVCS_ERR_UNKNOWN           = AVCS_ERR_OFFSET + 0x200,  // unkown error.
+    AVCS_ERR_SERVICE_DIED,                                 // avcodec service died
+    AVCS_ERR_CREATE_AVCODEC_SUB_SERVICE_FAILED,            // create avcodec sub service failed.
+    AVCS_ERR_CREATE_MUXER_SUB_SERVICE_FAILED,              // create muxer sub service failed.
+    AVCS_ERR_CREATE_DEMUXER_SUB_SERVICE_FAILED,            // create demuxer sub service failed.
+    AVCS_ERR_INVALID_STATE,                                // the state is not support this operation.
+    AVCS_ERR_UNSUPPORT,                                    // unsupport interface.
+    AVCS_ERR_UNSUPPORT_AUD_SRC_TYPE,                       // unsupport audio source type.
+    AVCS_ERR_UNSUPPORT_AUD_SAMPLE_RATE,                    // unsupport audio sample rate.
+    AVCS_ERR_UNSUPPORT_AUD_CHANNEL_NUM,                    // unsupport audio channel.
+    AVCS_ERR_UNSUPPORT_AUD_ENC_TYPE,                       // unsupport audio encoder type.
+    AVCS_ERR_UNSUPPORT_AUD_PARAMS,                         // unsupport audio params(other params).
+    AVCS_ERR_UNSUPPORT_VID_SRC_TYPE,                       // unsupport video source type.
+    AVCS_ERR_UNSUPPORT_VID_ENC_TYPE,                       // unsupport video encoder type.
+    AVCS_ERR_UNSUPPORT_VID_PARAMS,                         // unsupport video params(other params).
+    AVCS_ERR_UNSUPPORT_CONTAINER_TYPE,                     // unsupport container format type.
+    AVCS_ERR_UNSUPPORT_PROTOCOL_TYPE,                      // unsupport protocol type.
+    AVCS_ERR_UNSUPPORT_VID_DEC_TYPE,                       // unsupport video decoder type.
+    AVCS_ERR_UNSUPPORT_AUD_DEC_TYPE,                       // unsupport audio decoder type.
+    AVCS_ERR_UNSUPPORT_STREAM,                             // internal data stream error.
+    AVCS_ERR_UNSUPPORT_FILE,                               // this appears to be a text file.
+    AVCS_ERR_UNSUPPORT_SOURCE,                             // unsupport source type.
+    AVCS_ERR_AUD_RENDER_FAILED,                            // audio render failed.
+    AVCS_ERR_AUD_ENC_FAILED,                               // audio encode failed.
+    AVCS_ERR_VID_ENC_FAILED,                               // video encode failed.
+    AVCS_ERR_AUD_DEC_FAILED,                               // audio decode failed.
+    AVCS_ERR_VID_DEC_FAILED,                               // video decode failed.
+    AVCS_ERR_MUXER_FAILED,                                 // stream avmuxer failed.
+    AVCS_ERR_DEMUXER_FAILED,                               // stream demuxer or parser failed.
+    AVCS_ERR_OPEN_FILE_FAILED,                             // open file failed.
+    AVCS_ERR_FILE_ACCESS_FAILED,                           // read or write file failed.
+    AVCS_ERR_START_FAILED,                                 // audio/video start failed.
+    AVCS_ERR_PAUSE_FAILED,                                 // audio/video pause failed.
+    AVCS_ERR_STOP_FAILED,                                  // audio/video stop failed.
+    AVCS_ERR_SEEK_FAILED,                                  // audio/video seek failed.
+    AVCS_ERR_NETWORK_TIMEOUT,                              // network timeout.
+    AVCS_ERR_NOT_FIND_CONTAINER,                           // not find a demuxer.
+    AVCS_ERR_DATA_SOURCE_IO_ERROR,                         // avcodec data source IO failed.
+    AVCS_ERR_DATA_SOURCE_OBTAIN_MEM_ERROR,                 // avcodec data source get mem failed.
+    AVCS_ERR_DATA_SOURCE_ERROR_UNKNOWN,                    // avcodec data source error unknow.
+    AVCS_ERR_EXTEND_START      = AVCS_ERR_OFFSET + 0xF000, // extend err start.
+} AVCodecServiceErrCode;
 
 // avcodec api error code
-enum AVCodecServiceExtErrCode : ErrCode {
-    AVCSERR_EXT_OK = 0,
-    AVCSERR_EXT_NO_MEMORY = 1,           // no memory.
-    AVCSERR_EXT_OPERATE_NOT_PERMIT = 2,  // opertation not be permitted.
-    AVCSERR_EXT_INVALID_VAL = 3,         // invalid argument.
-    AVCSERR_EXT_IO = 4,                  // IO error.
-    AVCSERR_EXT_TIMEOUT = 5,             // network timeout.
-    AVCSERR_EXT_UNKNOWN = 6,             // unknown error.
-    AVCSERR_EXT_SERVICE_DIED = 7,        // avcodec service died.
-    AVCSERR_EXT_INVALID_STATE = 8,       // the state is not support this operation.
-    AVCSERR_EXT_UNSUPPORT = 9,           // unsupport interface.
-    AVCSERR_EXT_EXTEND_START = 100,      // extend err start.
-};
+typedef enum AVCodecServiceExtErrCode : ErrCode {
+    AVCS_ERR_EXT_OK = 0,
+    AVCS_ERR_EXT_NO_MEMORY = 1,           // no memory.
+    AVCS_ERR_EXT_OPERATE_NOT_PERMIT = 2,  // opertation not be permitted.
+    AVCS_ERR_EXT_INVALID_VAL = 3,         // invalid argument.
+    AVCS_ERR_EXT_IO = 4,                  // IO error.
+    AVCS_ERR_EXT_TIMEOUT = 5,             // network timeout.
+    AVCS_ERR_EXT_UNKNOWN = 6,             // unknown error.
+    AVCS_ERR_EXT_SERVICE_DIED = 7,        // avcodec service died.
+    AVCS_ERR_EXT_INVALID_STATE = 8,       // the state is not support this operation.
+    AVCS_ERR_EXT_UNSUPPORT = 9,           // unsupport interface.
+    AVCS_ERR_EXT_EXTEND_START = 100,      // extend err start.
+} AVCodecServiceExtErrCode;
+
+// avcodec northbound interface error code
+typedef enum OH_AVCodecErrCode {
+    AVC_ERR_OK = 0,
+    AVC_ERR_NO_MEMORY = 1,           // no memory.
+    AVC_ERR_OPERATE_NOT_PERMIT = 2,  // opertation not be permitted.
+    AVC_ERR_INVALID_VAL = 3,         // invalid argument.
+    AVC_ERR_IO = 4,                  // IO error.
+    AVC_ERR_TIMEOUT = 5,             // network timeout.
+    AVC_ERR_UNKNOWN = 6,             // unknown error.
+    AVC_ERR_SERVICE_DIED = 7,        // avcodec service died.
+    AVC_ERR_INVALID_STATE = 8,       // the state is not support this operation.
+    AVC_ERR_UNSUPPORT = 9,           // unsupport interface.
+    AVC_ERR_EXTEND_START = 100,      // extend err start.
+} OH_AVCodecErrCode;
 
 __attribute__((visibility("default"))) std::string AVCSErrorToString(AVCodecServiceErrCode code);
 __attribute__((visibility("default"))) std::string AVCSExtErrorToString(AVCodecServiceExtErrCode code);
