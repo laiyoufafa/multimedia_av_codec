83f03abd773f6a2c3710cb5402db88a80e6b9598
zhanghongran <zhanghongran@huawei.com>
2023-04-25 14:39:57 +0800

    【修改说明】修改编译报错
    Signed-off-by: zhanghongran <zhanghongran@huawei.com>

--- services/engine/base/codecbase.cpp
+++ services/engine/base/codecbase.cpp
@@ -17,7 +17,7 @@
 #include "avcodec_log.h"
 
 namespace {
-    constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN, "CodecBase"}
+    constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN, "CodecBase"};
 }
 
 namespace OHOS {
