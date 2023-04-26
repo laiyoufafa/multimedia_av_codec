e53d28f3f0e0db6b75326b2feb3aa3591f7cf4b6
peng <yangpeng43@huawei.com>
2023-04-26 15:53:58 +0800

    【修改说明】修改文件头时间和头文件宏
    Signed-off-by: peng <yangpeng43@huawei.com>

--- test/nativedemo/audio_demo/avcodec_audio_encoder_inner_demo.h
+++ test/nativedemo/audio_demo/avcodec_audio_encoder_inner_demo.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef AVCODEC_VDEC_DEMO_H
-#define AVCODEC_VDEC_DEMO_H
+#ifndef AVCODEC_AUDIO_ENCODER_INNER_DEMO_H
+#define AVCODEC_AUDIO_ENCODER_INNER_DEMO_H
 
 #include <atomic>
 #include <fstream>
@@ -83,4 +83,4 @@ private:
 } // InnerAudioDemo
 } // namespace AV_Codec
 } // namespace OHOS
-#endif // AVCODEC_VDEC_DEMO_H
+#endif // AVCODEC_AUDIO_ENCODER_INNER_DEMO_H
