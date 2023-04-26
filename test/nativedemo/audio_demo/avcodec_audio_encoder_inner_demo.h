30849737bad645335c7a19d1de7f296d4b3a9e1b
peng <12329789+peng5233@user.noreply.gitee.com>
2023-04-26 14:59:34 +0800

    【修改说明】 添加SA配置和音频测试代码
    Signed-off-by: peng <yangpeng43@huawei.com>

---
+++ test/nativedemo/audio_demo/avcodec_audio_encoder_inner_demo.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AVCODEC_VDEC_DEMO_H
+#define AVCODEC_VDEC_DEMO_H
+
+#include <atomic>
+#include <fstream>
+#include <thread>
+#include <queue>
+#include <string>
+#include "avcodec_common.h"
+#include "avcodec_audio_decoder.h"
+#include "nocopyable.h"
+
+namespace OHOS {
+namespace Media {
+namespace InnerAudioDemo {
+class ADecSignal {
+public:
+    std::mutex inMutex_;
+    std::mutex outMutex_;
+    std::condition_variable inCond_;
+    std::condition_variable outCond_;
+    std::queue<uint32_t> inQueue_;
+    std::queue<uint32_t> outQueue_;
+};
+
+class ADecDemoCallback : public AVCodecCallback, public NoCopyable {
+public:
+    explicit ADecDemoCallback(std::shared_ptr<ADecSignal> signal);
+    virtual ~ADecDemoCallback() = default;
+
+    void OnError(AVCodecErrorType errorType, int32_t errorCode) override;
+    void OnOutputFormatChanged(const Format &format) override;
+    void OnInputBufferAvailable(uint32_t index) override;
+    void OnOutputBufferAvailable(uint32_t index, AVCodecBufferInfo info, AVCodecBufferFlag flag) override;
+
+private:
+    std::shared_ptr<ADecSignal> signal_;
+};
+
+class ADecInnerDemo : public NoCopyable {
+public:
+    ADecInnerDemo() = default;
+    virtual ~ADecInnerDemo() = default;
+    void RunCase();
+
+private:
+    int32_t CreateDec();
+    int32_t Configure(const Format &format);
+    int32_t Start();
+    int32_t Stop();
+    int32_t Flush();
+    int32_t Reset();
+    int32_t Release();
+    void InputFunc();
+    void OutputFunc();
+
+    std::atomic<bool> isRunning_ = false;
+    std::unique_ptr<std::ifstream> testFile_;
+    std::unique_ptr<std::thread> inputLoop_;
+    std::unique_ptr<std::thread> outputLoop_;
+    std::shared_ptr<AVCodecAudioDecoder> audioDec_;
+    std::shared_ptr<ADecSignal> signal_;
+    std::shared_ptr<ADecDemoCallback> cb_;
+    bool isFirstFrame_ = true;
+    int64_t timeStamp_ = 0;
+    uint32_t frameCount_ = 0;
+};
+} // InnerAudioDemo
+} // namespace AV_Codec
+} // namespace OHOS
+#endif // AVCODEC_VDEC_DEMO_H
