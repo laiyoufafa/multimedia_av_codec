c54d16e529e7de450de71e3d455d345c73580e70
ltc <lintaicheng1@huawei.com>
2023-05-04 20:43:48 +0800

    修改avmuxer单元测试框架
    Signed-off-by: ltc <lintaicheng1@huawei.com>

---
+++ test/unittest/avmuxer_test/avmuxer_unit_test.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef AVMUXER_UNIT_TEST_H
+#define AVMUXER_UNIT_TEST_H
+
+#include "gtest/gtest.h"
+#include "avmuxer_sample.h"
+
+namespace OHOS {
+namespace Media {
+class AVMuxerUnitTest : public testing::Test {
+public:
+    // SetUpTestCase: Called before all test cases
+    static void SetUpTestCase(void);
+    // TearDownTestCase: Called after all test case
+    static void TearDownTestCase(void);
+    // SetUp: Called before each test cases
+    void SetUp(void);
+    // TearDown: Called after each test cases
+    void TearDown(void);
+
+protected:
+    std::shared_ptr<AVMuxerSample> avmuxer_ {nullptr};
+    int32_t fd_ {-1};
+    std::shared_ptr<std::ifstream> avDataFile_ {nullptr};
+    uint8_t buffer_[3] = {'a', 'a', 'a'};
+};
+} // namespace Media
+} // namespace OHOS
+#endif // AVMUXER_UNIT_TEST_H
