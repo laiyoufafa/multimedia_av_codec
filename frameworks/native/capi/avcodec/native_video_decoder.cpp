f562163fc520adcbe2a984acdf338b74f054374a
li-jianchao1993 <lijianchao9@huawei.com>
2023-05-16 21:33:14 +0800

    【修改说明】解决Video Decoder输出设为surface卡死问题
    Signed-off-by: li-jianchao1993 <lijianchao9@huawei.com>

--- frameworks/native/capi/avcodec/native_video_decoder.cpp
+++ frameworks/native/capi/avcodec/native_video_decoder.cpp
@@ -44,6 +44,7 @@ struct VideoDecoderObject : public OH_AVCodec {
     std::atomic<bool> isFlushing_ = false;
     std::atomic<bool> isStop_ = false;
     std::atomic<bool> isEOS_ = false;
+    bool isOutputSurfaceMode_ =false;
 };
 
 class NativeVideoDecoderCallback : public AVCodecCallback {
@@ -114,7 +115,12 @@ public:
         bufferAttr.size = info.size;
         bufferAttr.offset = info.offset;
         bufferAttr.flags = flag;
-        callback_.onNeedOutputData(codec_, index, nullptr, &bufferAttr, userData_);
+        OH_AVMemory *data = nullptr;
+        if(videoDecObj->isOutputSurfaceMode_ == false) {
+            data = GetOutputData(codec_, index);
+            CHECK_AND_RETURN_LOG(data != nullptr, "Data is nullptr, get output data failed");
+        }
+        callback_.onNeedOutputData(codec_, index, data, &bufferAttr, userData_);
     }
 
     void StopCallback()
@@ -157,7 +163,7 @@ private:
         CHECK_AND_RETURN_RET_LOG(videoDecObj->videoDecoder_ != nullptr, nullptr, "Video decoder is nullptr!");
 
         std::shared_ptr<AVSharedMemory> memory = videoDecObj->videoDecoder_->GetOutputBuffer(index);
-        CHECK_AND_RETURN_RET_LOG(memory != nullptr, nullptr, "Memory is nullptr, get output buffer failed!");
+        CHECK_AND_RETURN_RET_LOG(memory != nullptr, nullptr, "Video decoder output data is nullptr!");
 
         for (auto &memoryObj : videoDecObj->memoryObjList_) {
             if (memoryObj->IsEqualMemory(memory)) {
@@ -353,6 +359,7 @@ OH_AVErrCode OH_VideoDecoder_SetSurface(OH_AVCodec *codec, OHNativeWindow *windo
 
     int32_t ret = videoDecObj->videoDecoder_->SetOutputSurface(window->surface);
     CHECK_AND_RETURN_RET_LOG(ret == AVCS_ERR_OK, AV_ERR_OPERATE_NOT_PERMIT, "Video decoder set output surface failed!");
+    videoDecObj->isOutputSurfaceMode_ = true;
 
     return AV_ERR_OK;
 }
