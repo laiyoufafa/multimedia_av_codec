5afe8c7c741ed7e17e5ea2bfe779a4ecbcbaaa45
gao_ziyu <gaoziyu@huawei.com>
2023-05-31 10:39:42 +0800

    fix 8000k mp3 decode problem
    Signed-off-by: gao_ziyu <gaoziyu@huawei.com>

--- services/engine/codec/audio/decoder/audio_ffmpeg_mp3_decoder_plugin.cpp
+++ services/engine/codec/audio/decoder/audio_ffmpeg_mp3_decoder_plugin.cpp
@@ -105,7 +105,7 @@ int32_t AudioFFMpegMp3DecoderPlugin::GetInputBufferSize() const
 int32_t AudioFFMpegMp3DecoderPlugin::GetOutputBufferSize() const
 {
     int32_t maxSize = (sampleRate / SAMPLE_RATE_RATIO + BUFFER_DIFF) * channels * sizeof(short);
-	int32_t minSize = MIN_OUTBUF_SIZE * channels * sizeof(short);
+    int32_t minSize = MIN_OUTBUF_SIZE * channels * sizeof(short);
     if (maxSize < minSize) {
         maxSize = minSize;
     }
