74ea979bee3182486f3fe8b564d3483987698bdc
yangjunhui10 <yangjunhui10@huawei.com>
2023-06-11 02:15:18 +0000

    demuxer crash fix
    Signed-off-by: yangjunhui10 <yangjunhui10@huawei.com>

--- services/engine/plugin/plugins/demuxer/ffmpeg_demuxer/ffmpeg_demuxer_plugin.cpp
+++ services/engine/plugin/plugins/demuxer/ffmpeg_demuxer/ffmpeg_demuxer_plugin.cpp
@@ -189,6 +189,15 @@ int32_t FFmpegDemuxerPlugin::SelectTrackByID(uint32_t trackIndex)
         AVCODEC_LOGE("trackIndex is invalid! Just have %{public}d tracks in file", formatContext_.get()->nb_streams);
         return AVCS_ERR_INVALID_VAL;
     }
+    AVStream* avStream = formatContext_->streams[trackIndex];
+    if (avStream->codecpar->codec_type != AVMEDIA_TYPE_AUDIO && avStream->codecpar->codec_type != AVMEDIA_TYPE_VIDEO) {
+        AVCODEC_LOGE("unsupport stream type");
+        return AVCS_ERR_INVALID_VAL;
+    }
+    if (avStream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO && avStream->codecpar->codec_id == AV_CODEC_ID_RAWVIDEO) {
+        AVCODEC_LOGE("unsupport raw video");
+        return AVCS_ERR_INVALID_VAL;
+    }
     auto index = std::find_if(selectedTrackIds_.begin(), selectedTrackIds_.end(),
                               [trackIndex](uint32_t selectedId) {return trackIndex == selectedId;});
     if (index == selectedTrackIds_.end()) {
@@ -295,17 +304,10 @@ int32_t FFmpegDemuxerPlugin::ConvertAVPacketToSample(AVStream* avStream, std::sh
     if (avStream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
         frameSize = static_cast<uint64_t>(samplePacket->pkt->size);
     } else if (avStream->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
-        if (avStream->codecpar->codec_id == AV_CODEC_ID_RAWVIDEO) {
-            AVCODEC_LOGE("unsupport raw video");
-            return AVCS_ERR_UNSUPPORT_STREAM;
-        }
         if (avbsfContext_) {
             ConvertAvcOrHevcToAnnexb(*(samplePacket->pkt));
         }
         frameSize = static_cast<uint64_t>(samplePacket->pkt->size);
-    } else {
-        AVCODEC_LOGE("unsupport stream type");
-        return AVCS_ERR_UNSUPPORT_VID_PARAMS;
     }
   
     bufferInfo.size = static_cast<int32_t>(frameSize);
